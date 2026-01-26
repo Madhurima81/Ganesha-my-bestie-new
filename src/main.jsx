20ae4bf1199ccc8f3474be0fa3778ce5b78ec890
gautam <gautamsbox@gmail.com>
2026-01-26 13:42:02 +0530

    reverted latest fixes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import AppV2 from './AppV2.jsx'
 import AppV3 from './AppV3.jsx'
+
+//import AppV1 from './AppV1.jsx'
+
 import './index.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AppV3/>
+    <App/>
   </React.StrictMode>,
 )
